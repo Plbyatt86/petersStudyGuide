a2313035946fa26455c827fae049fd8889f32f61
Plbyatt86 <byatt.pete@gmail.com>
2024-02-07 15:46:59 -0500

    new practice

---
+++ javascript/practice.js
@@ -0,0 +1,24 @@
+// Define the shopping cart array with items
+// Function to calculate the total price of the shopping cart// Define the shopping cart array with items
+const shoppingCart = [
+    {
+      name: "T-shirt",
+      price: 15,
+      quantity: 2
+    },
+    {
+      name: "Jeans",
+      price: 30,
+      quantity: 1
+    },
+    {
+      name: "Sneakers",
+      price: 50,
+      quantity: 1,
+      options: {
+        size: "10",
+        color: "black"
+      }
+    }
+  ];
+
